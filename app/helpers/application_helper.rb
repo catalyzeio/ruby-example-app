07a0c322e81dafebf8ed5e499995f0843efd2b69
Anthony Pleshek <anthony.pleshek@gmail.com>
2015-05-05 13:35:21 -0500

    Added some example logging messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  log = Logger.new(STDOUT)
+
+  log.info("Info Message: Showing how logging works")
+  log.debug("Debug message: Logging is working")
+  log.warn("Warn Message: This app doesn't actually do anything!")
 end
